ebeb6ce8e83d2b7aea6ca13fb1b944d2beb24179
drfrostongithub <52021027+drfrostongithub@users.noreply.github.com>
2020-08-27 16:43:44 +0700

    Update job-router.js

--- routes/job-router.js
+++ routes/job-router.js
@@ -3,6 +3,13 @@ const router = express.Router()
 const JobController = require('../controllers/job-controller.js')
 
 
+routers.get('/' , JobController.getJob)
+routers.get('/add', JobController.addJob);
+routers.post('/add', JobController.addJobPost);
+routers.get('/:id/edit', JobController.editJob);
+routers.post('/:id/edit', JobController.editJobPost);
+routers.get('/:id/delete', JobController.getJobDelete);
+
 
 
 module.exports = router
