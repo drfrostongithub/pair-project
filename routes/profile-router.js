8b479ac02de6328a23b221792b072f2c6e486e12
drfrostongithub <52021027+drfrostongithub@users.noreply.github.com>
2020-08-27 16:45:18 +0700

    Update profile-router.js

--- routes/profile-router.js
+++ routes/profile-router.js
@@ -3,6 +3,12 @@ const router = express.Router()
 const ProfileController = require('../controllers/profile-controller.js')
 
 
+routers.get('/' , ProfileController.getProfile)
+routers.get('/add', ProfileController.addProfile);
+routers.post('/add', ProfileController.addProfilePost);
+routers.get('/:id/edit', ProfileController.editProfile);
+routers.post('/:id/edit', ProfileController.editProfilePost);
+routers.get('/:id/delete', ProfileController.getProfileDelete);
 
 
 module.exports = router
