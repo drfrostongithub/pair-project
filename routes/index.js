9933e8efe0fdaa1f06fe17d6bec019cfe5f8f8b0
drfrostongithub <52021027+drfrostongithub@users.noreply.github.com>
2020-08-27 16:41:12 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router()
 const jobRouter = require('./job-router.js')
 const profileRouter = require('./profile-router')
 
+router.use(`/`)
+router.use(`/profile`, profileRouter)
+router.use(`/job`,jobRouter)
 
 module.exports = router
